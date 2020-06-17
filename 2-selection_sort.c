9a636c1444880c2026bdee83480a2758a7546d31
diego-9407 <60360505+diego-9407@users.noreply.github.com>
2020-06-17 14:05:10 -0500

    [add]: selection method implemented

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Sort a dlinked list using bubble sort
+ * @list: head of the list
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t index = 0;
+	size_t index2;
+	size_t min;
+	int temp;
+
+	while (index < size)
+	{
+		min = index;
+		index2 = index;
+		while (index2 < size)
+		{
+			if (array[index2] < array[min])
+				min = index2;
+			index2++;
+		}
+		temp = array[index];
+		array[index] = array[min];
+		array[min] = temp;
+		print_array(array, size);
+		index++;
+	}
+}
